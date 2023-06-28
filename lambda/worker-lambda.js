6383a6d3d1881ba24de5aefa3fcc3379679facca
abhi-192 <contactabhi.2020@gmail.com>
2023-06-28 13:55:42 +0530

    modify invoke function for worker lambda

--- lambda/worker-lambda.js
+++ lambda/worker-lambda.js
@@ -15,6 +15,43 @@ const dynamo = DynamoDBDocumentClient.from(client);
 const tableName = "score";
 
 const updateScore = async(body) => {
+
+    const fileName = this.requestBody.fileName;
+    const foldername = 'problem-'+body.contestID+'-'+body.quesID ;
+    const params = {
+        Bucket: 's3-bucket-problems',
+        Key: `${foldername}/${fileName}`,
+    };
+    const s3 = new S3();
+    const { s3ResponseBody } = await s3.getObject(params).promise()
+
+    let score = s3ResponseBody.score;
+    let res = await dynamo.send(
+        new QueryCommand({
+            TableName: tableName,
+            KeyConditionExpression:
+                "userID = :userID",
+            ExpressionAttributeValues: {
+                ":userID" : body.userID
+            },
+            ConsistentRead: true,
+        })
+    );
+    res = res.Item;
+    score += res.score;
+    await dynamo.send(
+        new UpdateCommand({
+            TableName: tableName,
+            Key: {
+                userID: body.userID,
+            },
+            UpdateExpression: "set score = :score",
+            ExpressionAttributeValues: {
+                ":score": score,
+            },
+            ReturnValues: "ALL_NEW",
+        })
+    )
 };
 
 export const handler = async (queueUrl = getQueueURL(SQS_QUEUE_NAME)) => {
