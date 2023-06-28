8f46f2e476073f1047dd0b401560893823769515
abhi-192 <contactabhi.2020@gmail.com>
2023-06-28 13:32:25 +0530

    evaluate lambda modify

--- lambda/evaluate-lambda.js
+++ lambda/evaluate-lambda.js
@@ -29,37 +29,6 @@ const getQueueURL = async (queueName) => {
 const SQS_QUEUE_URL = getQueueURL(SQS_QUEUE_NAME);
 
 const receiveMessage = (queueUrl) =>
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import {
-    DynamoDBDocumentClient,
-    UpdateCommand
-} from "@aws-sdk/lib-dynamodb";
-import {
-    ReceiveMessageCommand,
-    DeleteMessageCommand,
-    GetQueueUrlCommand,
-    SQSClient,
-} from "@aws-sdk/client-sqs";
-
-const sqsClient = new SQSClient({});
-const SQS_QUEUE_NAME = 'submissionQ';
-const SQS_QUEUE_NAME2 = 'scoreQ';
-
-const client = new DynamoDBClient({});
-const dynamo = DynamoDBDocumentClient.from(client);
-const tableName = "submission";
-
-const getQueueURL = async (queueName) => {
-    const command = new GetQueueUrlCommand({ QueueName: queueName });
-    const response = await sqsClient.send(command);
-    console.log('Submission Queue URL', response);
-
-    return response;
-};
-
-const SQS_QUEUE_URL = getQueueURL(SQS_QUEUE_NAME);
-
-const receiveMessage = (queueUrl) =>
     client.send(
         new ReceiveMessageCommand({
             AttributeNames: ["SentTimestamp"],
@@ -108,6 +77,25 @@ const processMessage = async (body) => {
 
     await dynamo.send(command);
 
+    // updating solution status - if solution is not correct
+    // errorReturned is mistake in code
+
+    let errorReturned = ""
+    command = new UpdateCommand({
+        TableName: tableName,
+        Key: {
+            submissionID: body.submissionID,
+        },
+        UpdateExpression: "set status = :status",
+        ExpressionAttributeValues: {
+            ":status": "NOT_OK",
+            ":comment": errorReturned,
+        },
+        ReturnValues: "ALL_NEW",
+    });
+
+    await dynamo.send(command);
+
     // check if points are already awarded to user or not
 
     await dynamo.send(
@@ -133,24 +121,15 @@ const processMessage = async (body) => {
     )
 
     if(body.weight === 0) return
-};
 
-export const handler = async (queueUrl = SQS_QUEUE_URL) => {
-    const { Messages } = await receiveMessage(queueUrl);
+    // push to scoreQ
 
-    if (Messages) {
-        for (const m of Messages) {
-            console.log('Message received by submissionQ',m.Body)
-            await processMessage(m.Body)
-            await client.send(
-                new DeleteMessageCommand({
-                    QueueUrl: queueUrl,
-                    ReceiptHandle: m.ReceiptHandle,
-                })
-            );
-        }
-    }
-};
+    const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+    sqs.sendMessage({
+        MessageBody: JSON.stringify(body),
+        QueueUrl: getQueueURL(SQS_QUEUE_NAME2),
+    }, (err, data) => (err) ? console.log("Error", err) : console.log("Successfully pushed to scoreQ", data));
+
 };
 
 export const handler = async (queueUrl = SQS_QUEUE_URL) => {
