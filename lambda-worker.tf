9eb7538611b8eefe695f5b38b42b2bc01e9e6563
abhi-192 <contactabhi.2020@gmail.com>
2023-06-28 13:50:09 +0530

    add iam role policy attachment for worker lambda

--- lambda-worker.tf
+++ lambda-worker.tf
@@ -13,3 +13,13 @@ resource "aws_iam_role" "iam_for_worker_lambda" {
   assume_role_policy = data.aws_iam_policy_document.worker_assume_role.json
   name               = "iam_for_worker_lambda"
 }
+
+resource "aws_iam_role_policy_attachment" "worker_lambda_policy" {
+  for_each = tomap({
+    "one" = aws_iam_policy.worker-lambda-policy.arn,
+    "two" = "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"
+  })
+
+  role       = aws_iam_role.iam_for_worker_lambda.name
+  policy_arn = each.value
+}
